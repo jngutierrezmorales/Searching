cd92164a41a238a60933e6026d412b624e151ba8
Juan Gutierrez <juan.gutierrez@auro-group.com>
2022-11-09 11:58:43 +0100

    Initial commit

---
+++ Searching/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Searching
+//
+//  Created by Juan Gutierrez on 8/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    
+    var body: some View {
+        NavigationView {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+        }
+    }
+    
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
+        } else {
+            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
